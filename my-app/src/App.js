e50825aa64a03e7923361c46487c384091f59bae
Lucien Tran <12878930@student.uts.edu.au>
2019-08-20 18:03:12 +1000

    Started first project with create-react-native rather than expo

---
+++ my-app/src/App.js
@@ -0,0 +1,53 @@
+/*import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
+export default class FlexDimensionsBasics extends Component {
+  render() {
+    return (
+      // Try removing the `flex: 1` on the parent View.
+      // The parent will not have dimensions, so the children can't expand.
+      // What if you add `height: 300` instead of `flex: 1`?
+      <View style={{height: 650}}>
+        <View style={{height: 300}}>
+          <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+          <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+          <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+        </View>
+
+        <View style={{flex:2}}>
+          <View style={{flex:1, backgroundColor: 'blue'}} />
+          <View style={{flex:2, backgroundColor: 'purple' }} />
+        </View>
+      </View>
+    );
+  }
+}
